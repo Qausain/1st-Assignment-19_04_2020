511d19dd6963d9f63f36adb94f8b8fe064e8d7f6
Qausain <64030938+Qausain@users.noreply.github.com>
2020-04-20 21:59:44 +0500

    Add files via upload

---
+++ 19_4_2020.py
@@ -0,0 +1,21 @@
+#1st Assignment of Sir Inam-Ul- Haq by Qausain Manzoor Dated: 19-4-2020.
+
+print("Addition")
+a = int(input("Enter 1st number "))
+b = int(input("Enter 2nd number "))
+print(a + b)
+
+print("Multiplication")
+a = int(input("Enter 1st number "))
+b = int(input("Enter 2nd number "))
+print(a * b)
+
+print("Subtraction")
+a = int(input("Enter 1st number "))
+b = int(input("Enter 2nd number "))
+print(a - b)
+
+print("Division")
+a = int(input("Enter 1st number "))
+b = int(input("Enter 2nd number "))
+print(a / b)
